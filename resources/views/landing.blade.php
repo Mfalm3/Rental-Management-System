82cd6f252ffc572e0a4d34be7b19d36c2203f3fd
Mfalm3 <waitha28ka@gmail.com>
2021-03-23 14:08:02 +0300

    Initialize landing page template

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta description="">
+    <meta author="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('APP_NAME') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
