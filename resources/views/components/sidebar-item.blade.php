39a15b16828914e044ece630d292e6ec4090bf7a
Mfalm3 <waitha28ka@gmail.com>
2021-04-06 11:22:54 +0300

    Add active state for links

--- resources/views/components/sidebar-item.blade.php
+++ resources/views/components/sidebar-item.blade.php
@@ -1,12 +1,22 @@
-@props(['to','color','fontawesome','location'])
+@props(['to','color','fontawesome','location','segment'])
 
 {{--<li class="mr-3 flex-1">--}}
 {{--    <a href="{{ $to }}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-{{ $color }}-500">--}}
 {{--        <i class="{{ $fontawesome }} pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">{{$slot}}</span>--}}
 {{--    </a>--}}
 {{--</li>--}}
+@php
+ $link = (request()->segment(1) == $segment)
+? "block py-1 md:py-3 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-".$color."-500 border-".$color."-500"
+: "block py-1 md:py-3 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-".$color."-500";
+
+$fa = (request()->segment(1) == $segment)
+? "text-".$color."-500"
+: ""
+@endphp
+
 <li class="mr-3 sm:mt-2 sm:mr-0 sm:text-left sm:px-8">
-    <a href="{{ $to }}" class="block py-1 md:py-3 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-{{ $color }}-500">
-        <i class="{{ $fontawesome }} pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">{{$slot}}</span>
+    <a href="{{ $to }}" {{ $attributes->merge(['class'=> $link]) }}>
+        <i class="{{ $fontawesome }} pr-0 md:pr-3" {{ $attributes->merge(['class'=>$fa]) }}></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">{{$slot}}</span>
     </a>
 </li>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,11 +4,11 @@
 
     <div class="flex flex-col md:flex-row">
         <x-sidebar>
-            <x-sidebar-item to="/dashboard" color="pink" fontawesome="fas fa-home">Dashboard</x-sidebar-item>
-            <x-sidebar-item to="/properties" path="properties" color="blue" fontawesome="fas fa-city">Properties</x-sidebar-item>
-            <x-sidebar-item to="#" color="purple" fontawesome="fas fa-chart-pie">Reports</x-sidebar-item>
-            <x-sidebar-item to="#" color="red" fontawesome="fas fa-cog">Manage Users</x-sidebar-item>
-            <x-sidebar-item to="#" color="green" fontawesome="fas fa-file">Statement</x-sidebar-item>
+            <x-sidebar-item to="/dashboard" segment="dashboard" color="pink" fontawesome="fas fa-home">Dashboard</x-sidebar-item>
+            <x-sidebar-item to="/properties" segment="properties" color="blue" fontawesome="fas fa-city">Properties</x-sidebar-item>
+            <x-sidebar-item to="#" color="purple" segment="report" fontawesome="fas fa-chart-pie">Reports</x-sidebar-item>
+            <x-sidebar-item to="#" color="red" segment="users" fontawesome="fas fa-cog">Manage Users</x-sidebar-item>
+            <x-sidebar-item to="#" color="green" segment="ui" fontawesome="fas fa-file">Statement</x-sidebar-item>
 
         </x-sidebar>
 
