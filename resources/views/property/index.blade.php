7a19cd90a0148ac5d754fdcec4401f335604edd7
Mfalm3 <waitha28ka@gmail.com>
2021-04-08 09:07:09 +0300

    Add index property view

--- resources/views/property/index.blade.php
+++ resources/views/property/index.blade.php
@@ -2,5 +2,18 @@
 
 @section('dash-content')
 
+    <div class="bg-gray-800 pt-3">
+        <div class="bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
+            <h3 class="font-bold pl-2">Manage Properties</h3>
+        </div>
+    </div>
 
+    <div class="flex flex-wrap justify-center">
+        @foreach($properties as $property)
+            <x-listing-card
+                name="{{ $property->name }}"
+                location="{{ $property->location }}"
+                property="{{ $property->name }}"/>
+        @endforeach
+    </div>
 @endsection
