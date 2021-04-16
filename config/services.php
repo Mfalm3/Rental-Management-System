d3440874bc058f403330bd99372616d9ce1e23f2
Mfalm3 <waitha28ka@gmail.com>
2021-04-16 09:10:16 +0300

    Add Cloudinary configs

--- config/services.php
+++ config/services.php
@@ -30,4 +30,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'cloudinary' => [
+        'cloud' => [
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+        ]
+    ]
 ];
