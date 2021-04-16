ad920f50b336eb68cc4b0b19d86e84aca88dbe89
Mfalm3 <waitha28ka@gmail.com>
2021-04-16 09:13:17 +0300

    Update Property save method

--- app/Http/Requests/PropertyRequest.php
+++ app/Http/Requests/PropertyRequest.php
@@ -3,8 +3,11 @@
 namespace App\Http\Requests;
 
 use App\Models\Property;
+use Cloudinary\Api\Exception\ApiError;
+use Cloudinary\Cloudinary;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Str;
+use Cloudinary\Api\Upload\UploadApi;
 
 class PropertyRequest extends FormRequest
 {
@@ -35,7 +38,33 @@ class PropertyRequest extends FormRequest
     public function save()
     {
         $uuid = (string) Str::uuid();
+        if($this->hasFile('images')){
+            $images = $this->file('images');
 
-       return Property::create(array_merge($this->request->all(), ['uuid'=>$uuid]));
+            $cloudinary = app()->make(Cloudinary::class);
+
+            $fileName = $this->request->get('name');
+
+            for($i=0; $i < count($images); $i++){
+                $fileNameToStore[$i] = Str::of($fileName)->slug()->append($i+1);
+
+                try {
+                    $upload_responses[] = $cloudinary->uploadApi()->upload($images[$i]->getRealPath(),[
+                        'folder' => 'rms',
+                        'use_filename' => true,
+                        'public_id' => $fileNameToStore[$i]
+                    ])->getArrayCopy();
+                } catch (ApiError $e) {
+                    echo $e;
+                }
+
+            }
+            foreach ($upload_responses as $upload_response){
+                $links[] = ['url' => $upload_response['secure_url']];
+            }
+        }
+//dd($links);
+        $property =  Property::create(array_merge($this->request->all(), ['uuid'=>$uuid]));
+        $property->images()->createMany($links);
     }
 }
