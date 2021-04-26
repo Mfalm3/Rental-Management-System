3500ce567bdd44f6e2b8ab606beae7fa22bdffec
Mfalm3 <waitha28ka@gmail.com>
2021-04-26 15:31:15 +0300

    Register relation morphMap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 use Cloudinary\Cloudinary;
 
@@ -27,5 +28,12 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(Cloudinary::class, function (){
             return new Cloudinary(config('services.cloudinary'));
         });
+
+        // Relation morph map
+        Relation::morphMap([
+            'tenant' => 'App\Models\Tenant',
+            'agent' => 'App\Models\Agent',
+            'landlord' => 'App\Models\Landlord',
+        ]);
     }
 }
