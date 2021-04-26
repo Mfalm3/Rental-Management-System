1b646cf292a72da847e173a7ff245f492852e66d
Mfalm3 <waitha28ka@gmail.com>
2021-04-26 16:17:12 +0300

    Add create new user button

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('dash-content')
 
+    <div class="bg-gray-800 pt-3">
+        <div class="bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white flex justify-between">
+            <h3 class="font-bold pl-2">Manage Users</h3>
+            <a href="{{ route('users.create') }}" class="bg-blue-600 rounded p-2 ring-2 ring-blue-400 text-sm transform hover:-translate-y-0.5 hover:ring-blue-500">Add new user</a>
+        </div>
+    </div>
+
     <div class="container">
         <div class="p-4 m-4">
             <div class="bg-white">
