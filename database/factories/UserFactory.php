d858d123b449f01a84f03b4eafe5e34a29c678ee
Mfalm3 <waitha28ka@gmail.com>
2021-04-27 10:58:33 +0300

    Refactor morph relation logic

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,10 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -37,7 +35,7 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'typeable_id' => $typeable->id,
-            'typeable_type' => get_class($typeable)
+            'typeable_type' => $userType
         ];
     }
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('typeable_id');
+            $table->unsignedBigInteger('typeable_id');
             $table->string('typeable_type');
             $table->rememberToken();
             $table->timestamps();
