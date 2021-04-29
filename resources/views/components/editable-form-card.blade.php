20d18173349d328b1811ccf44b7f7c1373d30bd4
Mfalm3 <waitha28ka@gmail.com>
2021-04-29 09:46:21 +0300

    Add transition to the cancel button

--- resources/views/components/editable-form-card.blade.php
+++ resources/views/components/editable-form-card.blade.php
@@ -1,8 +1,15 @@
 <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 bg-gray-100" x-data="editableForm()">
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         <div class="p-4 float-right space-x-3">
-            <x-button class="bg-blue-500 hover:bg-blue-300" @click="edit=true">Edit</x-button>
-            <x-button @click="edit=false" x-show="edit==true">Cancel</x-button>
+            <x-button class="bg-blue-500 hover:bg-blue-700" @click="edit=true">Edit</x-button>
+            <x-button @click.debounce="edit=false" x-show="edit==true"
+                      x-transition:enter="transition ease-in-out duration-700"
+                      x-transition:enter-start="opacity-0 transform scale-90"
+                      x-transition:enter-end="opacity-100 transform scale-100"
+                      x-transition:leave="transition ease-in duration-300"
+                      x-transition:leave-start="opacity-100 transform scale-100"
+                      x-transition:leave-end="opacity-0 transform scale-90"
+            >Cancel</x-button>
         </div>
 
         {{ $slot }}
