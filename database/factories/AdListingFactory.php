160e1bace57ecbb5e1d1495f146926d40a3207fc
Mfalm3 <waitha28ka@gmail.com>
2021-05-06 08:21:10 +0300

    Add database migration and factory for ad listing

---
+++ database/factories/AdListingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AdListing;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdListingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AdListing::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
