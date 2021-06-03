df76e358758c3e3e0db6e44c50e7b3467b83f427
Mfalm3 <waitha28ka@gmail.com>
2021-06-03 18:03:14 +0300

    add blade resource files for AdListing model

--- resources/views/ad/create.blade.php
+++ resources/views/ad/create.blade.php
@@ -6,17 +6,17 @@
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
 
             <div class="flex flex-wrap">
-                <div class="w-full xl:w-1/3 m-auto">
-                    <h3 class="text-center my-4">Add new Ad listing</h3>
+                <div class="w-full xl:p-10 m-auto">
+                    <h3 class="text-center my-4">Create a new Ad listing</h3>
                     <hr class="mb-3">
 
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                    <form method="POST" action="/properties" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('ad.store') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <x-label for="name" :value="__('Property Name')" />
+                            <x-label for="name" :value="__('Ad Description')" />
 
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autocomplete="off"/>
+                            <textarea id="name" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autocomplete="off"></textarea>
                         </div>
                         <div class="mb-3">
                             <x-label for="name" :value="__('Location')" />
@@ -24,18 +24,19 @@
                             <x-input id="location" class="block mt-1 w-full" type="text" name="location" :value="old('location')" required autocomplete="off"/>
                         </div>
                         <div class="mb-3">
-                            <x-label for="ac_no" :value="__('Account Number')" />
+                            <x-label for="name" :value="__('Price')" />
 
-                            <x-input id="ac_no" class="block mt-1 w-full" type="text" name="account_number" :value="old('account_number')" autocomplete="off"/>
+                            <x-input id="location" class="block mt-1 w-full" type="number" name="price" :value="old('price')" required autocomplete="off"/>
                         </div>
                         <div class="mb-3">
-                            <x-label for="landlord" :value="__('Property Owner')" />
+                            <x-label for="ac_no" :value="__('Contact Number')" />
 
-
+                            <x-input id="ac_no" class="block mt-1 w-full" type="text" name="contact" :value="old('contact')" autocomplete="off"/>
                         </div>
+
                         <div class="mb-3">
                             <x-label for="image" :value="__('Image files')"></x-label>
-                            <x-input type="file" name="images[]" multiple/>
+                            <x-input type="file" name="images[]" multiple required/>
                         </div>
                         <div class="mt-4 float-right">
                             <x-button>Submit</x-button>
--- resources/views/ad/index.blade.php
+++ resources/views/ad/index.blade.php
@@ -2,9 +2,18 @@
 
 @section('dash-content')
 
-    <ul>
+    <div class="bg-gray-800 pt-3">
+        <div class="bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white flex justify-between">
+            <h3 class="font-bold pl-2">Manage Ad Listings</h3>
+            <a href="{{ route('ad.create') }}" class="bg-blue-600 rounded p-2 ring-2 ring-blue-400 text-sm transform hover:-translate-y-0.5 hover:ring-blue-500">Create new Ad</a>
+        </div>
+    </div>
+    <div class="flex flex-wrap m-5">
+
         @foreach($ads as $ad)
-            <li>{{ $ad->id }} {{ substr_replace($ad->description,'...', 0, 35) }} {{ $ad->location }}</li>
+            <x-adlistcard :ad="$ad" />
         @endforeach
-    </ul>
+
+    </div>
+
 @endsection
